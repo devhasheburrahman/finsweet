a5664cae9e26de9553d916596efa6fd404602860
HASHEBUR RAHMAN <you@example.com>
2023-07-04 13:46:56 +0600

    font

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Team = () => {
     return (
         <div className='text-center mt-32'>
-            <p style={{ lineHeight: '28px' }} className='text-[#232536] text-[sen] text-3xl font-bold mb-4'>Join our team to be a part <br />of our story</p>
+            <p style={{ lineHeight: '28px' }} className='text-[#232536] font-[sen] text-3xl font-bold mb-4'>Join our team to be a part <br />of our story</p>
             <p className='text-[#6D6E76] font-[Inter] text-base mb-8'>Lorem ipsum dolor sit amet, consectetur adipiscing <br /> elit, sed do eiusmod tempor incididunt.</p>
-            <button className='bg-[#FFD050]  px-12 py-4  rounded-md font-bold text-[#232536] mb-32'>Join Now</button>
+            <button className='bg-[#FFD050]  font-[sen] px-12 py-4  rounded-md font-bold text-[#232536] mb-32'>Join Now</button>
         </div>
     );
 };
